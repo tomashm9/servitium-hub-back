695dbcb1ea6e0a09b7a8e1b4dd3864b9d5a8682d
Tomás Mendes <tomashm16@gmail.com>
2024-08-01 10:17:38 +0200

    feat: add Base entity

---
+++ domain/src/main/java/com/thm/gw/entities/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.thm.gw.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@MappedSuperclass
+@Getter
+public class BaseEntity<T extends Serializable> {
+
+    @Id
+    @GeneratedValue
+    private T id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Setter
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    @Setter
+    private LocalDateTime updatedAt;
+}
