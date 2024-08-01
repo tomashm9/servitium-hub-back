aae02ef4ce44bc1f9168e513985641c6a6b60238
Tomás Mendes <tomashm16@gmail.com>
2024-08-01 12:24:01 +0200

    feat: implement spring secutiry configuration + Jwt

---
+++ config/src/main/java/com/thm/gw/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.thm.gw.utils;
+
+public class Constants {
+
+    // AUTH
+    public static final String AUTH_ENDPOINT = "/v1/auth/";
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String JWT_TOKEN_PREFIX = "Bearer ";
+
+    // ROLES
+    public static final String ADMIN_ROLE = "ADMIN";
+    public static final String OWNER_ROLE = "OWNER";
+    public static final String MANAGER_ROLE = "MANAGER";
+    public static final String CLIENT_ROLE = "CLIENT";
+}
