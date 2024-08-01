4977ef2bc9f31449783fc2abf7b17e9ac9d1a0d9
Tomás Mendes <tomashm16@gmail.com>
2024-08-01 03:38:49 +0200

    first commit

---
+++ common/src/test/java/com/thm/gw/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thm.gw.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ controller/src/main/java/com/thm/gw/ServitiumHubBackApplication.java
@@ -0,0 +1,13 @@
+package com.thm.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServitiumHubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServitiumHubBackApplication.class, args);
+    }
+
+}
